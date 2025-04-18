6b8a727a73ffd449af689aae4db67393925952c0
Diksha Vasuja <vasujadiksha@gmail.com>
2025-04-18 19:30:15 +0530

    Restore working version of server.js and package.json with booking functionality

--- scm_backend/server.js
+++ scm_backend/server.js
@@ -34,9 +34,5 @@ app.post('/book', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`✅ Server is running on http://localhost:${PORT}`);
-<<<<<<< HEAD
+    console.log(✅ Server is running on http://localhost:${PORT});
 });
-=======
-});
->>>>>>> cc48060cce6c6ea8d987fc12cc17ff672699aec4
